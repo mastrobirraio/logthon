5dca4f9cde9c647a672f6b01eb483061fbf4a96a
Pino Matranga <matrangagiuseppe99@gmail.com>
2019-07-21 23:10:26 +0200

    1.6.0 Travis CI file (#4)
    * created travis file for CI
    
    * updated setup for 1.6.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requirements = {
 
 setup(
     name=NAME,
-    version='1.5.3',
+    version='1.6.0',
     author='Giuseppe "mastrobirraio" Matranga',
     author_email='matrangagiuseppe99@gmail.com',
     maintainer='Giuseppe "mastrobirraio Matranga',
