1e7ce37beaf6924a7433104396e98eaad8902f83
mastrobirraio <pino.matranga1999@gmail.com>
2019-07-21 18:57:29 +0200

    created setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='logthon',
+    version='1.0.0',
+    author='Giuseppe "mastrobirraio" Matranga',
+    author_email='matrangagiuseppe99@gmail.com',
+    license='GPLv3',
+    description='A simple logger for Python',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mastrobirraio/logthon',
+    packages=find_packages,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ]
+)
