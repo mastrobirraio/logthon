6cb50ce922ec56badc85f426b93d277c38a2a7ae
Pino Matranga <matrangagiuseppe99@gmail.com>
2019-07-21 19:56:56 +0200

    1.1.1 initial work (#1)
    * defined development requirements
    
    * imported loghton from gitshub
    
    * added IDEs stuffs to .gitignore
    
    * created setup.py
    
    * installed twine
    
    * fixed setup.py
    
    * fixed setup.py
    
    * written docstring using PEP257
    
    * updated setup.py for 1.1.0
    
    * added .pypirc to .gitignore and added .pypirc example file
    
    * finished README.md and created CONTRIBUTING.md
    
    * updated setup.py for 1.1.1

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# logthon
-A simple python logger
+# Logthon
+
+A simple logger for Python
+
+## Getting Started
+
+These instructions will install the logger to your machine.
+
+### Prerequisites
+
+* Python3
+* PIP3
+
+### Installation
+
+```
+pip3 install logthon
+```
+
+## Usage
+
+### Import
+
+```
+from logthon.logthon import Logthon as Logger
+
+Logthon = Logger()
+```
+
+### Info level
+
+```
+Logthon.info('This is an info log')
+```
+
+### Warn level
+
+```
+Logthon.warn('This is a warn log')
+```
+
+### Error level
+
+```
+Logthon.error('This is an error log')
+```
+
+### Success level
+
+```
+Logthon.info('This is a success log')
+```
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on code of conduct, and the process for submitting pull requests.
+
+## License
+
+This project is licensed under the GNU General Public License v3 (GPLv3), read [LICENSE](LICENSE) for details 
+
+## Author
+
+* **Giuseppe "mastrobirraio" Matranga** - *Initial work* - [Github](https://github.com/mastrobirraio)
---
+++ logthon/logthon.py
@@ -0,0 +1,86 @@
+# logthon.py
+# -*- coding: utf-8 -*-
+
+"""
+Logthhon
+~~~~~~~~
+
+Loghton is a simple logger for Python
+"""
+
+from datetime import datetime
+
+"""OUTPUT FORMAT"""
+STD_FORMAT = '[{timestamp}] - {level}: {message}'
+
+"""COLORS"""
+YELLOW_FORMAT = '1;33;40m'
+GREY_FORMAT = '1;30;40m'
+RED_FORMAT = '1;31;40m'
+GREEN_FORMAT = '1;32;40m'
+
+"""LEVELS"""
+INFO_LEVEL = 'INFO'
+WARN_LEVEL = 'WARNING'
+ERRO_LEVEL = 'ERROR'
+SUCC_LEVEL = 'SUCCESS'
+
+
+class Logthon:
+
+    def __init__(self):
+        """Set the format global variable as class attribute"""
+        self.format = STD_FORMAT
+
+    def compose_message(self, level, message):
+        """Compose the text message
+
+        :param level: the log level one of the defined global LEVELS
+        :param message: the message to log
+        :return: the formatted string
+        """
+        return self.format.format(
+            timestamp=datetime.now(),
+            level=level,
+            message=message)
+
+    def compose_output(self, color, message):
+        """Compose the output message
+
+        :param color: the string color according to log level
+        :param message: the message to log
+        :return:
+        """
+        return '\033[{}{}\033[00m'.format(color, message)
+
+    def info(self, message):
+        """Print a log using INFO_LEVEL
+
+        :param message: the message to log
+        """
+        message = self.compose_message(INFO_LEVEL, message)
+        print(self.compose_output(GREY_FORMAT, message))
+
+    def warn(self, message):
+        """Print a log using WARN_LEVEL
+
+        :param message: the message to log
+        """
+        message = self.compose_message(WARN_LEVEL, message)
+        print(self.compose_output(YELLOW_FORMAT, message))
+
+    def error(self, message):
+        """Print a log using ERRO_LEVEL
+
+        :param message: the message to log
+        """
+        message = self.compose_message(ERRO_LEVEL, message)
+        print(self.compose_output(RED_FORMAT, message))
+    
+    def success(self, message):
+        """Print a log using SUCC_LEVEL
+
+        :param message: the message to log
+        """
+        message = self.compose_message(SUCC_LEVEL, message)
+        print(self.compose_output(GREEN_FORMAT, message))
