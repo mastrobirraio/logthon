aaa8495a2b4a988687f54e9bfa205b78e7344e6c
mastrobirraio <pino.matranga1999@gmail.com>
2019-07-23 09:31:23 +0200

    refactored test_logthon.py

--- tests/test_logthon.py
+++ tests/test_logthon.py
@@ -9,9 +9,10 @@ from logthon import logthon as ln
 from tests.utils import compose_log
 
 Logthon = ln.Logthon()
+FREEZE_DATE = '2012-01-14'
 
 
-@freeze_time('2012-01-14')
+@freeze_time(FREEZE_DATE)
 def test_info_level(capsys):
     message = 'This is an info test log'
     Logthon.info(message)
@@ -19,7 +20,7 @@ def test_info_level(capsys):
     assert compose_log(ln.INFO_LEVEL, message) in captured.out
 
 
-@freeze_time('2012-01-14')
+@freeze_time(FREEZE_DATE)
 def test_warn_level(capsys):
     message = 'This is a warn test log'
     Logthon.warn(message)
@@ -27,7 +28,7 @@ def test_warn_level(capsys):
     assert compose_log(ln.WARN_LEVEL, message) in captured.out
 
 
-@freeze_time('2012-01-14')
+@freeze_time(FREEZE_DATE)
 def test_error_level(capsys):
     message = 'This is an error test log'
     Logthon.error(message)
@@ -35,7 +36,7 @@ def test_error_level(capsys):
     assert compose_log(ln.ERRO_LEVEL, message) in captured.out
 
 
-@freeze_time('2012-01-14')
+@freeze_time(FREEZE_DATE)
 def test_success_level(capsys):
     message = 'This is a success test log'
     Logthon.success(message)
@@ -43,7 +44,7 @@ def test_success_level(capsys):
     assert compose_log(ln.SUCC_LEVEL, message) in captured.out
 
 
-@freeze_time('2012-01-14')
+@freeze_time(FREEZE_DATE)
 def test_critical_level(capsys):
     message = 'This is a critical test log'
     Logthon.critical(message)
@@ -51,7 +52,7 @@ def test_critical_level(capsys):
     assert compose_log(ln.CRITICAL_LEVEL, message) in captured.out
 
 
-@freeze_time('2012-01-14')
+@freeze_time(FREEZE_DATE)
 def test_debug_level(capsys):
     message = 'This is a debug test log'
     Logthon.debug(message)
