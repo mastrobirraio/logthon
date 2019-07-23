6b8298d80a487eca27a631abb36262c4b298e339
mastrobirraio <pino.matranga1999@gmail.com>
2019-07-23 14:47:20 +0200

    written tests for logthon with save on default file

--- tests/test_logthon.py
+++ tests/test_logthon.py
@@ -4,6 +4,7 @@
 """Tests for `logthon` package"""
 
 import io
+import os
 import unittest.mock
 
 from freezegun import freeze_time
@@ -59,6 +60,64 @@ class TestLogthon(unittest.TestCase):
         message = 'This is a success test log'
         self.logthon.warn(message)
         self.assertIn(message, mock_stdout.getvalue())
+
+
+class TestLogthonDefaultFile(unittest.TestCase):
+
+    def setUp(self):
+        self.temp_file = '../logthon/logthon.log'
+        self.logthon = Logthon(save_log=True)
+
+    def _clean(self):
+        os.remove(self.temp_file)
+
+    @freeze_time(FREEZE_DATE)
+    def test_info_level(self):
+        message = 'This is an info test log'
+        self.logthon.info(message)
+        with open(self.temp_file) as log_file:
+            self.assertIn(message, log_file.read())
+        self._clean()
+
+    @freeze_time(FREEZE_DATE)
+    def test_warn_level(self):
+        message = 'This is a warn test log'
+        self.logthon.warn(message)
+        with open(self.temp_file) as log_file:
+            self.assertIn(message, log_file.read())
+        self._clean()
+
+    @freeze_time(FREEZE_DATE)
+    def test_error_level(self):
+        message = 'This is an error test log'
+        self.logthon.error(message)
+        with open(self.temp_file) as log_file:
+            self.assertIn(message, log_file.read())
+        self._clean()
+
+    @freeze_time(FREEZE_DATE)
+    def test_critical_level(self):
+        message = 'This is a critical test log'
+        self.logthon.critical(message)
+        with open(self.temp_file) as log_file:
+            self.assertIn(message, log_file.read())
+        self._clean()
+
+    @freeze_time(FREEZE_DATE)
+    def test_debug_level(self):
+        message = 'This is a debug test log'
+        self.logthon.debug(message)
+        with open(self.temp_file) as log_file:
+            self.assertIn(message, log_file.read())
+        self._clean()
+
+    @freeze_time(FREEZE_DATE)
+    def test_success_level(self):
+        message = 'This is a success test log'
+        self.logthon.success(message)
+        with open(self.temp_file) as log_file:
+            self.assertIn(message, log_file.read())
+        self._clean()
 
 
 if __name__ == '__main__':
