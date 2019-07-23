e71cbf43f9e726f05fb564d221e1c14f3010c530
mastrobirraio <pino.matranga1999@gmail.com>
2019-07-23 13:03:59 +0200

    migrated tests from pytest to unittest

--- tests/test_logthon.py
+++ tests/test_logthon.py
@@ -3,58 +3,63 @@
 
 """Tests for `logthon` package"""
 
+import io
+import unittest.mock
+
 from freezegun import freeze_time
 
-from logthon import logthon as ln
-from tests.utils import compose_log
+from logthon.logthon import Logthon
 
-Logthon = ln.Logthon()
 FREEZE_DATE = '2012-01-14'
 
 
-@freeze_time(FREEZE_DATE)
-def test_info_level(capsys):
-    message = 'This is an info test log'
-    Logthon.info(message)
-    captured = capsys.readouterr()
-    assert compose_log(ln.INFO_LEVEL, message) in captured.out
+class TestLogthon(unittest.TestCase):
+
+    def setUp(self):
+        self.logthon = Logthon()
+
+    @freeze_time(FREEZE_DATE)
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_info_level(self, mock_stdout):
+        message = 'This is an info test log'
+        self.logthon.info(message)
+        self.assertIn(message, mock_stdout.getvalue())
+
+    @freeze_time(FREEZE_DATE)
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_warn_level(self, mock_stdout):
+        message = 'This is a warn test log'
+        self.logthon.warn(message)
+        self.assertIn(message, mock_stdout.getvalue())
+
+    @freeze_time(FREEZE_DATE)
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_error_level(self, mock_stdout):
+        message = 'This is an error test log'
+        self.logthon.error(message)
+        self.assertIn(message, mock_stdout.getvalue())
+
+    @freeze_time(FREEZE_DATE)
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_critical_level(self, mock_stdout):
+        message = 'This is a critical test log'
+        self.logthon.critical(message)
+        self.assertIn(message, mock_stdout.getvalue())
+
+    @freeze_time(FREEZE_DATE)
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_debug_level(self, mock_stdout):
+        message = 'This is a debug test log'
+        self.logthon.debug(message)
+        self.assertIn(message, mock_stdout.getvalue())
+
+    @freeze_time(FREEZE_DATE)
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_success_level(self, mock_stdout):
+        message = 'This is a success test log'
+        self.logthon.warn(message)
+        self.assertIn(message, mock_stdout.getvalue())
 
 
-@freeze_time(FREEZE_DATE)
-def test_warn_level(capsys):
-    message = 'This is a warn test log'
-    Logthon.warn(message)
-    captured = capsys.readouterr()
-    assert compose_log(ln.WARN_LEVEL, message) in captured.out
-
-
-@freeze_time(FREEZE_DATE)
-def test_error_level(capsys):
-    message = 'This is an error test log'
-    Logthon.error(message)
-    captured = capsys.readouterr()
-    assert compose_log(ln.ERRO_LEVEL, message) in captured.out
-
-
-@freeze_time(FREEZE_DATE)
-def test_success_level(capsys):
-    message = 'This is a success test log'
-    Logthon.success(message)
-    captured = capsys.readouterr()
-    assert compose_log(ln.SUCC_LEVEL, message) in captured.out
-
-
-@freeze_time(FREEZE_DATE)
-def test_critical_level(capsys):
-    message = 'This is a critical test log'
-    Logthon.critical(message)
-    captured = capsys.readouterr()
-    assert compose_log(ln.CRITICAL_LEVEL, message) in captured.out
-
-
-@freeze_time(FREEZE_DATE)
-def test_debug_level(capsys):
-    message = 'This is a debug test log'
-    Logthon.debug(message)
-    captured = capsys.readouterr()
-    assert compose_log(ln.DEBUG_LEVEL, message) in captured.out
+if __name__ == '__main__':
+    unittest.main()
