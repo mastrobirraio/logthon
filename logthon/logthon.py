e656821f9ddec9e0548266749fd577ee09048fd8
mastrobirraio <pino.matranga1999@gmail.com>
2019-08-04 16:30:02 +0200

    set compose_message and compose_output as private methods. logthon.py

--- logthon/logthon.py
+++ logthon/logthon.py
@@ -55,7 +55,7 @@ class Logthon:
             with open(self._filename, 'a') as log_file:
                 log_file.write('{}\n'.format(message))
 
-    def compose_message(self, level, message):
+    def _compose_message(self, level, message):
         """Compose the text message
 
         :param level: the log level one of the defined global LEVELS
@@ -70,7 +70,7 @@ class Logthon:
         self._log_to_file(log_message)
         return log_message
 
-    def compose_output(self, color, message):
+    def _compose_output(self, color, message):
         """Compose the output message
 
         :param color: the string color according to log level
@@ -88,45 +88,45 @@ class Logthon:
 
         :param message: the message to log
         """
-        message = self.compose_message(INFO_LEVEL, message)
-        print(self.compose_output(LOG_LEVELS[INFO_LEVEL], message))
+        message = self._compose_message(INFO_LEVEL, message)
+        print(self._compose_output(LOG_LEVELS[INFO_LEVEL], message))
 
     def warn(self, message):
         """Print a log using WARN_LEVEL
 
         :param message: the message to log
         """
-        message = self.compose_message(WARN_LEVEL, message)
-        print(self.compose_output(LOG_LEVELS[WARN_LEVEL], message))
+        message = self._compose_message(WARN_LEVEL, message)
+        print(self._compose_output(LOG_LEVELS[WARN_LEVEL], message))
 
     def error(self, message):
         """Print a log using ERRO_LEVEL
 
         :param message: the message to log
         """
-        message = self.compose_message(ERRO_LEVEL, message)
-        print(self.compose_output(LOG_LEVELS[ERRO_LEVEL], message))
+        message = self._compose_message(ERRO_LEVEL, message)
+        print(self._compose_output(LOG_LEVELS[ERRO_LEVEL], message))
     
     def success(self, message):
         """Print a log using SUCC_LEVEL
 
         :param message: the message to log
         """
-        message = self.compose_message(SUCC_LEVEL, message)
-        print(self.compose_output(LOG_LEVELS[SUCC_LEVEL], message))
+        message = self._compose_message(SUCC_LEVEL, message)
+        print(self._compose_output(LOG_LEVELS[SUCC_LEVEL], message))
 
     def critical(self, message):
         """Print a log using CRITICAL_LEVEL
 
         :param message: the message to log
         """
-        message = self.compose_message(CRITICAL_LEVEL, message)
-        print(self.compose_output(LOG_LEVELS[CRITICAL_LEVEL], message))
+        message = self._compose_message(CRITICAL_LEVEL, message)
+        print(self._compose_output(LOG_LEVELS[CRITICAL_LEVEL], message))
 
     def debug(self, message):
         """Print a log using DEBUG_LEVEL
 
         :param message: the message to log
         """
-        message = self.compose_message(DEBUG_LEVEL, message)
-        print(self.compose_output(LOG_LEVELS[DEBUG_LEVEL], message))
+        message = self._compose_message(DEBUG_LEVEL, message)
+        print(self._compose_output(LOG_LEVELS[DEBUG_LEVEL], message))
