1a339d8e9fa89c0aa2ee6e473f16ecb5a3e28fad
mastrobirraio <pino.matranga1999@gmail.com>
2019-08-19 13:23:32 +0200

    updated setup for 2.1.4

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = {
 
 setup(
     name=NAME,
-    version='2.1.3',
+    version='2.1.4',
     author='Giuseppe "mastrobirraio" Matranga',
     author_email='matrangagiuseppe99@gmail.com',
     maintainer='Giuseppe "mastrobirraio Matranga',
