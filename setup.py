2e2db6732486701db6177cd33cb2c91c36e61fdd
Giuseppe Matranga <matrangagiuseppe99@gmail.com>
2021-07-05 12:19:08 +0200

    2.5.0: optional debug logs print

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = {
 
 setup(
     name=NAME,
-    version='2.4.2',
+    version='2.5.0',
     author='Giuseppe "mastrobirraio" Matranga',
     author_email='matrangagiuseppe99@gmail.com',
     maintainer='Giuseppe "mastrobirraio" Matranga',
